80167bc291c08a2c8f5e96b78a73a89f40037121
jondavidjohn <me@jondavidjohn.com>
2011-10-05 19:50:51 -0500

    Files in place

---
+++ README.php
@@ -0,0 +1,9 @@
+AUTH FROM SCRATCH
+=================
+
+1. Download latest version of both Codeigniter and Ion Auth
+2. Place Ion Auth files in 'application/third_party/ion_auth'
+3. Create your Database, for this the name will be 'auth_example'
+4. Import ion_auth.sql into your database
+
+... Follow the commits
